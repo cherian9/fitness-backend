5c11269b4fd6cb2f48dd585276cc30f531dec8e2
Cherian Chirackal Joseph <c7cherianjoseph@gmail.com>
2026-08-08 01:44:32 +0100

    connected to postgres database

--- src/controllers/weightController.js
+++ src/controllers/weightController.js
@@ -10,42 +10,73 @@ let weights = [
             date: "2026-08-06"
         }
     ];
-const getWeights = (req, res) => {
-    res.json(weights);
+const pool = require("../config/db");
+
+const getWeights = async (req, res) => {
+
+    try {
+
+        const result = await pool.query(
+            "SELECT * FROM weights ORDER BY id ASC"
+        );
+
+        res.json(result.rows);
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            message: "Database error"
+        });
+
+    }
+
 };
 
-const addWeight = (req, res) => {
 
-    const { weight, date} = req.body;
+const addWeight = async (req, res) => {
 
-    if (!weight || !date) {
-        return res.status(400).json({
-            message: "weight and date are required"
-        })
-    }
+    try {
 
-     if (typeof weight !== "number") {
-        return res.status(400).json({
-            message: "Weight must be a number."
+        const { weight, date } = req.body;
+
+        if (!weight || !date) {
+            return res.status(400).json({
+                message: "Weight and date are required."
+            });
+        }
+
+        if (typeof weight !== "number") {
+            return res.status(400).json({
+                message: "Weight must be a number."
+            });
+        }
+
+        if (weight <= 0) {
+            return res.status(400).json({
+                message: "Weight must be greater than zero."
+            });
+        }
+
+        const result = await pool.query(
+            `INSERT INTO weights (weight, date)
+             VALUES ($1, $2)
+             RETURNING *`,
+            [weight, date]
+        );
+
+        res.status(201).json(result.rows[0]);
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            message: "Database error"
         });
+
     }
-
-    if (weight <= 0) {
-        return res.status(400).json({
-            message: "Weight must be greater than zero."
-        });
-    }
-
-    const newWeight = {
-    id: weights.length + 1,
-    weight,
-    date
-    };
-    //newWeight.id = weights.length + 1;
-
-    weights.push(newWeight);
-
-    res.status(201).json(newWeight);
 
 };
 
